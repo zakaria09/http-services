c4bfd9f2fac900ba2ac0ecca2163033f4106f36b
Zakaria Arr <zakaria.09@hotmail.co.uk>
2019-06-16 00:07:06 +0100

    creating data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { url } from 'inspector';
 
 @Component({
   selector: 'app-posts',
@@ -11,12 +12,23 @@ export class PostsComponent implements OnInit {
   constructor(private Http: HttpClient) {}
 
   posts: any;
+  url: string = 'https://jsonplaceholder.typicode.com/posts';
 
   ngOnInit() {
-    this.Http.get('https://jsonplaceholder.typicode.com/posts')
+    this.Http.get(this.url)
       .subscribe(response => {
         this.posts = response;
       });
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = {title: input.value}
+    input.value = "";
+    this.Http.post(this.url, post)
+    .subscribe(res => {
+      post['id'] = res['id'];
+      this.posts.splice(0, 0, post)
+    })
   }
 
 }
