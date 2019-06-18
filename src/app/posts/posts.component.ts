54e18fa28184fd5291b09b319dfcd082718ca60a
Zakaria Arr <zakariaarr@Claudias-iMac.local>
2019-06-18 10:09:39 +0100

    implemented error messages bad http requests

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -17,6 +17,10 @@ export class PostsComponent implements OnInit {
     this.postservice.getPost()
       .subscribe(response => {
         this.posts = response;
+      }, 
+      error => {
+        alert('An error has occured!')
+        console.log(error);
       });
   }
 
@@ -24,24 +28,41 @@ export class PostsComponent implements OnInit {
     let post = {title: input.value}
     input.value = "";
     this.postservice.createPost(post)
-    .subscribe(res => {
+    .subscribe(
+    res => {
       post['id'] = res['id'];
       this.posts.splice(0, 0, post)
+    }, 
+    error => {
+      alert('An error has occured!')
     })
   }
 
   updatePost(post) {
     this.postservice.updatePost(post)
-      .subscribe(result => {
+      .subscribe(
+      result => {
         console.log(result);
+      }, 
+      error => {
+        alert('An error has occured!')
       })
   }
 
   deletePost(post) {
     this.postservice.deletePost(post)
-      .subscribe(result => {
+      .subscribe(
+      result => {
         let index = this.posts.indexOf(post);
         this.posts.splice(index, 1);
+      }, 
+      (error: Response) => {
+        if (error.status === 404) {
+          alert('This post has alraedy been deleted!')
+        } else {
+          alert('An error has occured!')
+          console.log(error)
+        }
       })
   }
 
